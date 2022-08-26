7da48a9e4f36871f0e98399008b83716dff1a73a
Janarthan S <84957980+janarthansubburaj@users.noreply.github.com>
2022-08-26 15:49:39 +0530

    initial commit

---
+++ SwiftUI-Day1/SwiftUI-Day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Day1
+//
+//  Created by Janarthan S on 26/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Day1/SwiftUI-Day1/SwiftUI_Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Day1App.swift
+//  SwiftUI-Day1
+//
+//  Created by Janarthan S on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
